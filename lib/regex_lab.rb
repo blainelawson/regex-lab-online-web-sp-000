195a1c34bcd0023b19e44b3e94568948169e288f
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-11 21:27:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -28,7 +28,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  binding.pry
+  # binding.pry
   if phone.scan(/\d/).length == 10
     return true
   else
